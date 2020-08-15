ed0e35a21a281451f739f6ffb5ac8de4c1247f5f
Saadi Save <saadisave@gmail.com>
2020-08-15 22:09:26 +0530

    commit

--- aqical/home/pollutants.py
+++ aqical/home/pollutants.py
@@ -1,14 +1,7 @@
-from typing import Dict
+from typing import Dict, NewType
 from systems import *
 aqiindex = Dict[str, int]
-class pollutants:
-    '''All values in ppm'''
-    def __init__(self) -> None:
-        self.__pm: float
-        self.__pm10: float
-        self.__co: float
-        self.__no2: float
-        self.__o3: float
-class aqi(pollutants):
-    def __init__(self) -> None:
+pin = Dict[float, str]
+class inp:
+    def __init__(self, polldict: pin) -> None:
         pass
