8fdcc06f3858c1a1a9d2b4617c1f04ecb2718b20
vsjain2004 <vjrainuuu@gmail.com>
2020-08-15 11:05:31 +0000

    commit4

--- aqical/home/application.py
+++ aqical/home/application.py
@@ -22,3 +22,7 @@ app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
