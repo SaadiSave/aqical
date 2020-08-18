326c50cabad20467c711fdf2eb90a5f6fb1dd99f
Saadi Save <saadisave@gmail.com>
2020-08-18 17:48:56 +0530

    commit

--- aqical/home/application.py
+++ aqical/home/application.py
@@ -3,10 +3,24 @@ from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
 from functools import wraps
+
+# at line 45, this error comes up. Not imp, but would I suggest adhering to the typed
+# Assume that the lang is statically typed 
+# Argument of type "() -> Text | None" cannot be assigned to parameter "p0" of type "_VT"
+# Type "() -> Text | None" is not compatible with bound type "(*args: Unknown, **kwargs: Unknown) -> NoReturn | (*args: Unknown, **kwargs: Unknown) -> (str | ByteString | Dict[Text, Any] | Response | _WSGICallable | Tuple[str | ByteString | Dict[Text, Any] | Response | _WSGICallable, str | int, Dict[Any, Any] | List[Tuple[Any, Any]]] | Tuple[str | ByteString | Dict[Text, Any] | Response | _WSGICallable, str | int] | Tuple[str | ByteString | Dict[Text, Any] | Response | _WSGICallable, Dict[Any, Any] | List[Tuple[Any, Any]]])" for TypeVar "_VT"
+# Function return type "Text | None" is not compatible with type "NoReturn"
+# Function return type "Text | None" is not compatible with type "str | ByteString | Dict[Text, Any] | Response | _WSGICallable | Tuple[str | ByteString | Dict[Text, Any] | Response | _WSGICallable, str | int, Dict[Any, Any] | List[Tuple[Any, Any]]] | Tuple[str | ByteString | Dict[Text, Any] | Response | _WSGICallable, str | int] | Tuple[str | ByteString | Dict[Text, Any] | Response | _WSGICallable, Dict[Any, Any] | List[Tuple[Any, Any]]]"Pylance (reportGeneralTypeIssues)
+
+# Import aqi thresholds using
+# import aqithresholds as aqi
+# accessing standard
+# aqi.EAQI
+
 # This is a message
 # I shall need the pollutant as str, value as float, and unit as str
 # Pollutant accepted values: pm2, pm10, co, no2, o3
-# units acc values: µg, ppm, ppb
+# units acc values: µg
+# show ppm and ppb, but convert them to µg and pass
 # which will all be eventually converted to ppb
 # Configure application
 app = Flask(__name__)
