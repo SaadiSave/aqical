c7177e35cd9b09c79a15973107ece9f063bb86c0
Saadi Save <saadisave@gmail.com>
2020-08-20 19:52:08 +0530

    Created class structure

--- home/aqimain.py
+++ home/aqimain.py
@@ -1,17 +1,33 @@
 import pandas as pd
+from typing import Dict
+# pdict = {
+#     'pm2' : 0.0,
+#     'pm10' : 0.0,
+#     'no2' : 0.0,
+#     'so2' : 0.0,
+#     'o3' : 0.0
+# }
 class aqi:
     def __init__(self, syst: str):
         try: assert((syst == 'eur') or (syst == 'ind') or (syst == 'usa'))
         except AssertionError: raise ValueError
-EAQI = pd.DataFrame({
-    'Pollutants' : ['PM2', 'PM10', 'NO2', 'SO2', 'O3'],
-    1 : [10, 20, 40, 100, 50],
-    2 : [20, 40, 90, 200, 100],
-    3 : [25, 50, 120, 350, 130],
-    4 : [50, 100, 230, 500, 240],
-    5 : [75, 150, 340, 750, 380],
-    6 : [800, 1200, 1000, 1250, 800]
-})
+        self.__system = syst
+        self.__res: str
+class inp(aqi):
+    def __init__(self, pdict: Dict[str, float]):
+        self.__vals = pdict
+class eaqi(inp):
+    def __init__(self):
+        self.__EAQI = pd.DataFrame({
+            'AQI' : ['Good', 'Fair', 'Moderate', 'Poor', 'Very Poor', 'Extremely Poor'],
+            'pm2' : [10, 20, 25, 50, 75, 800],
+            'pm10' : [20, 40, 50, 100, 150, 1200],
+            'no2' : [40, 90, 120, 230, 340, 1000],
+            'so2' : [100, 200, 350, 500, 750, 1250],
+            'o3' : [50, 100, 130, 240, 380, 800],
+        })
+    def setres(self):
+        pass
 #Tbe
 USAQI = pd.DataFrame({
     'Pollutants' : ['O3', 'NO2', 'SO2', 'PM10', 'PM2.5'],
