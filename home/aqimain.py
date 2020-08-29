c00cc759b0b0a1fa89141eb4bae9bb92188d023a
Saadi Save <saadisave@gmail.com>
2020-08-29 18:56:36 +0530

    Refined Class Structure

--- home/aqimain.py
+++ home/aqimain.py
@@ -15,32 +15,46 @@
 #     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 import pandas as pd
 from typing import Dict
-# Class input format
-# pdict = {
-#     'pm2' : 0.0,
-#     'pm10' : 0.0,
-#     'no2' : 0.0,
-#     'so2' : 0.0,
-#     'o3' : 0.0
-# }
+
 class aqi:
-    def __init__(self, syst: str):
-        try: assert((syst == 'eur') or (syst == 'ind') or (syst == 'usa'))
+    def __init__(self, syst: str, pm2: float, pm10: float, no2: float, o3: float, so2: float) -> None:
+        try: assert((syst == 'eur') or (syst == 'ind'))
         except AssertionError: raise ValueError
         self.__system = syst
+        self.__vals: Dict[str, float] = {
+            'pm2' : pm2,
+            'pm10' : pm10,
+            'no2' : no2,
+            'o3' : o3,
+            'so2' : so2
+        }
         self.__res: str
-class inp(aqi):
-    def __init__(self, pdict: Dict[str, float]):
-        self.__vals = pdict
-class eaqi(inp):
-    def __init__(self):
+    def getres(self) -> str:
+        return self.__res
+
+class eaqi(aqi):
+    def __init__(self) -> None:
         self.__EAQI = pd.DataFrame({
             'AQI' : ['Good', 'Fair', 'Moderate', 'Poor', 'Very Poor', 'Extremely Poor'],
             'pm2' : [10, 20, 25, 50, 75, 800],
             'pm10' : [20, 40, 50, 100, 150, 1200],
             'no2' : [40, 90, 120, 230, 340, 1000],
-            'so2' : [100, 200, 350, 500, 750, 1250],
             'o3' : [50, 100, 130, 240, 380, 800],
+            'so2' : [100, 200, 350, 500, 750, 1250]
         })
     def setres(self):
         for i in self.__vals: pass
+
+class naqi(aqi):
+    def __init__(self, co: float) -> None:
+        self.__NAQI = pd.DataFrame({
+            'AQI' : ['Good', 'Fair', 'Moderate', 'Poor', 'Very Poor', 'Extremely Poor'],
+            'pm2' : [10, 20, 25, 50, 75, 800],
+            'pm10' : [20, 40, 50, 100, 150, 1200],
+            'no2' : [40, 90, 120, 230, 340, 1000],
+            'o3' : [50, 100, 130, 240, 380, 800],
+            'so2' : [100, 200, 350, 500, 750, 1250]
+        })
+        self.__vals['co'] = co
+    def setres(self):
+        pass
