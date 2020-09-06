4c92d61fdfa9e0b7c17c09db8ab0f3e198b9b5f4
Saadi Save <saadisave@gmail.com>
2020-09-06 19:24:12 +0530

    Explicit declaration for s

--- home/application.py
+++ home/application.py
@@ -44,7 +44,7 @@ def home():
     if request.method == "GET":
         return render_template("home.html")
     else:
-        s = Dict[str, float]
+        s: Dict[str, float]
         casedict: Dict[str, str] = {
             'PM2.5' : 'pm2',
             'PM10' : 'pm10',
@@ -63,5 +63,4 @@ def home():
             s.update({
                 f : value
             })
-        AQI.aqi(None, s)
         return render_template("home.html", s = s)
