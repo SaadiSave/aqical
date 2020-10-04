43fd413f9c5a29e01a290ca04b3ebc4350d7ad1c
vsjain2004 <vjrainuuu@gmail.com>
2020-10-04 13:32:54 +0000

    changed NAQI values

--- home/main.py
+++ home/main.py
@@ -1,11 +1,11 @@
 # A file to test the prog
 from test import aqi
 O = aqi('eur', pdict={
-    'pm2' : 700,
-    'pm10' : 1100,
-    'so2' : 1100,
-    'no2' : 900,
-    'o3' : 700,
+    'pm2' : 50,
+    'pm10' : 100,
+    'so2' : 100,
+    'no2' : 100,
+    'o3' : 100,
 })
 O.setres()
 O.set_des()
